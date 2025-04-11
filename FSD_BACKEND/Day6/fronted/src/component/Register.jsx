1f14ddac42f669953df369e8efd15ebb0343e6d6
Vaishnavi Yadav <167222524+vaish2509@users.noreply.github.com>
2025-04-11 15:21:14 +0530

    Update Register.jsx

--- FSD_BACKEND/Day6/fronted/src/component/Register.jsx
+++ FSD_BACKEND/Day6/fronted/src/component/Register.jsx
@@ -6,7 +6,7 @@ const Register = () => {
         const name = e.target.name.value;
         const age = e.target.age.value;
         const users={name,age}
-       await axios.post('https://fsd-backend-elyx.onrender.com/users/',users)
+       await axios.post('http://localhost:9000/users/',users)
        alert("User registered")
     }
   return (
