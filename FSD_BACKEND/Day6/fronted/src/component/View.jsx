e4f1ab3ec2c3a9d62a73e2387fb406259a7364d5
Vaishnavi Yadav <167222524+vaish2509@users.noreply.github.com>
2025-04-11 15:37:27 +0530

    Update View.jsx

--- FSD_BACKEND/Day6/fronted/src/component/View.jsx
+++ FSD_BACKEND/Day6/fronted/src/component/View.jsx
@@ -7,7 +7,7 @@ const View = () => {
         handleview();
     },[])
     const handleview= async () => {
-        const res=await axios.get('http://localhost:9000/users');
+        const res=await axios.get('https://backend-fsd-5.onrender.com/users');
         setUsers(res.data);
         console.log(users);
     }
